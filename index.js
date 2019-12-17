ee2099e8adabfb84a8cb4c68015021d1597eb9e6
Mathis <mathis.schaller@etu.umontpellier.fr>
2019-12-17 17:44:12 +0100

    recuperation article par id

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const axios = require('axios')
 
 // Repositories
-const articles = require('./articles.js')
+const articlesRepository = require('./articles.js')
 
 // Constantes
 const PORT = process.env.PORT || 5000 // this is very important
@@ -20,43 +20,51 @@ const app = express()
 app.use(cors())
 
 app.listen(PORT, function () {
-  console.log('Server listening port ' + PORT)
+	console.log('Server listening port ' + PORT)
 })
 
 // Routes
 app.get('/', async function (req, res) {
-  const users = await getUtilisateurs();
-  res.json(users)
+	const users = await getUtilisateurs();
+	res.json(users)
 })
 
 // Tous les articles
 app.get('/articles', async function (req, res) {
-  const users = await articles.getArticles();
-  res.json(users)
+	const articles = await articlesRepository.getArticles();
+	res.json(articles)
 })
 
 // Un article
 app.get('/article/:id', async function (req, res) {
+	const id = req.params.id
+	const articles = await articlesRepository.getArticle(id);
+	res.json(articles)
 })
 
 // Ajouter article
 app.get('/article/add', async function (req, res) {
+	res.json({})
 })
 
 // Supprimer article
 app.get('/article/remove/:id', async function (req, res) {
+	res.json({})
 })
 
 // Modifier article
 app.get('/article/edit/:id', async function (req, res) {
+	res.json({})
+
 })
 
 // Creer compte
 app.get('/new-account', async function (req, res) {
+	res.json({})
 })
 
 // Se connecter
 app.get('/login', async function (req, res) {
-  const users = await articles.getArticles();
-  res.json(users)
+	const users = await articlesRepository.getArticles();
+	res.json(users)
 })
