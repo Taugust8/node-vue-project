4d8cdb97b3d8ae93815181117eeedf237b509ad0
Mathis <mathis.schaller@etu.umontpellier.fr>
2019-12-17 15:00:34 +0100

    Test de recuperation depuis heroku

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
+var cors = require('cors')
 const app = express()
 const PORT = process.env.PORT || 5000 // this is very important
 
+app.use(cors())
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.json({msg: 'tp node vue'})
 })
 
 app.listen(PORT, function () {
